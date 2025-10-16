0e0abe0099bfafdc8ec6fd96deddb78a99f26046
@ <oe@MacBook-Pro-.local>
2025-10-16 15:57:07 +0500

    New file added and modified

--- config.py
+++ config.py
@@ -4,4 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-DB_PATH = "discipline.db"
+
+# Render yoki lokal muhitda to‘g‘ri ishlashi uchun
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "discipline.db")
